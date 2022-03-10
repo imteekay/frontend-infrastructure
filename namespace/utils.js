7ea4b78c091de40edb382ae3109864f4f3aba099
imteekay <leandrotk100@gmail.com>
2022-03-10 13:38:44 -0300

    improve utils

---
+++ namespace/utils.js
@@ -0,0 +1,16 @@
+const Utils = {
+  Math: {
+    sum: function (num1, num2) {
+      return num1 + num2;
+    },
+    minus: function (num1, num2) {
+      return num1 - num2;
+    },
+    times: function (num1, num2) {
+      return num1 * num2;
+    },
+    multiply: function (num1, num2) {
+      return num1 / num2;
+    },
+  },
+};
