d490e4d39449f83fb0b3dd480fd02c1cc48db771
imteekay <leandrotk100@gmail.com>
2022-03-17 14:07:22 -0300

    script improvement

--- tree-shaking/src/index.js
+++ tree-shaking/src/index.js
@@ -6,29 +6,28 @@ const parser = new Parser('1');
 const modules = parser.parse();
 const treeShaker = new TreeShaker(modules);
 
-// make it one big bundle with new modules
-const moduleStringOptimized = treeShaker.modules
-  .map((m) => escodegen.generate(m))
-  .join('');
+function generateBundleString(modules, generateCode) {
+  return modules.map((module) => generateCode(module)).join('');
+}
 
-// also make one bundled version with the old modules
-// note: we have to specifiy the module prop on the module object
-const moduleStringUnshaked = treeShaker.unshaked
-  .map((u) => escodegen.generate(u.module))
-  .join('');
+function calculateOptimization(optimizedBundle, bundle) {
+  return Math.floor((1 - optimizedBundle.length / bundle.length) * 100);
+}
 
-// have a look at how different they look
-console.log('------ Bundle not optimized ------\n');
-console.log(moduleStringUnshaked, '\n');
-
-console.log('------ Bundle optimized ------\n');
-console.log(moduleStringOptimized);
-
-// let's count the characters
-//  do a naive comparison => in percent
-const impr = Math.floor(
-  (1 - moduleStringOptimized.length / moduleStringUnshaked.length) * 100,
+const optimizedBundle = generateBundleString(treeShaker.modules, (module) =>
+  escodegen.generate(module),
 );
 
-// IMPROVEMENT:  39% 🎉
-console.log(`\nImprovement: ${impr}% 🎉\n`);
+const bundle = generateBundleString(treeShaker.unshaked, (module) =>
+  escodegen.generate(module.module),
+);
+
+console.log('-------- Bundle not optimized ------\n');
+console.log(bundle, '\n');
+
+console.log('-------- Bundle optimized --------\n');
+console.log(optimizedBundle, '\n');
+
+const improvement = calculateOptimization(optimizedBundle, bundle);
+console.log('-------- Optimization --------\n');
+console.log(`\nImprovement: ${improvement}% 🎉\n`);
