39609fb592b7726be2023ebaf654cb2fe7b409af
imteekay <leandrotk100@gmail.com>
2022-03-17 13:57:01 -0300

    more improvements

--- tree-shaking/src/tree-shaker.js
+++ tree-shaking/src/tree-shaker.js
@@ -1,36 +1,31 @@
 export class TreeShaker {
   constructor({ importedModules, allModules }) {
     this.unshaked = allModules;
-    this.modules = this.shake(allModules, importedModules);
+    this.importedModules = importedModules;
+    this.modules = this.shake();
   }
 
-  shake(allModules, importedModules) {
-    // get all the values from the module map defined in Parser
-    // and turn them into an array
-    // btw make a copy of modules otherwise
-    // you won't be able to compare old vs new
-    return Array.from(allModules.entries()).map(([, { module: m }]) => {
-      const module = { ...m };
-      const { body } = module;
+  shake() {
+    return Object.values(this.unshaked).map(({ module }) => {
+      const moduleCopy = { ...module };
+      const { body } = moduleCopy;
       const shakedBody = [];
-      // traverse every body of every module
-      // look for export declarations &&
-      // if the exported name is in the array of imports
-      // we include it in our new body
-      // also we include everything else thats not an export
-      body.forEach((node) => {
+
+      for (const node of body) {
         if (node.type === 'ExportNamedDeclaration') {
-          node.declaration.declarations.forEach(({ id }) => {
-            if (importedModules.has(id.name)) {
+          for (const { id } of node.declaration.declarations) {
+            if (this.importedModules.has(id.name)) {
               shakedBody.push(node);
             }
-          });
+          }
         } else {
           shakedBody.push(node);
         }
-      });
-      module.body = shakedBody;
-      return module;
+      }
+
+      moduleCopy.body = shakedBody;
+
+      return moduleCopy;
     });
   }
 }
